3f1e6d4f32e849380d87bea366a30172f07c69dc
jimenezh <43828251+jimenezh@users.noreply.github.com>
2022-03-25 15:31:44 -0400

    Set up for python-bridge

---
+++ demos/web-mpc/partial_dec.js
@@ -0,0 +1,11 @@
+module.exports = async function (ciphertext) {
+// Requirements for using python TNO paillier package
+const assert = require('assert');
+const python = require('python-bridge');
+const py = python();
+
+
+
+
+
+}
